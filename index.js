34e7e8fcd8d0182960a8ef12d3303c91e172eb38
christina kaliora <develop@pirron.one>
2020-02-14 00:42:14 +0100

    added dynamiv port

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 // code away!
  const server = require("./server.js");
 
- server.listen(5000, () => {
+ const port = process.env.PORT || 5000;
+ server.listen(port, () => {
      console.log("\n *** Server running on http://localhost:5000 *** \n")
  })
